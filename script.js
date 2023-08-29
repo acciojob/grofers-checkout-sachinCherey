0f20b26c699046f3a2f5d29ab04961ed6b6e3b59
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-08-29 21:06:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+
+	  const getSumBtn = document.getElementById("getSumBtn");
+    const table = document.querySelector("table");
+
+    getSumBtn.addEventListener("click", () => {
+      const valueCells = document.querySelectorAll(".price");
+
+      let total = 0;
+
+      // Loop through each cell and add its value to the total
+      valueCells.forEach(cell => {
+        const cellValue = parseInt(cell.textContent);
+        if (!isNaN(cellValue)) {
+          total += cellValue;
+        }
+      });
+
+      // Create a new row and cell for the sum
+      const newRow = table.insertRow();
+      const newCell = newRow.insertCell();
+
+      // Set the cell's content to the total sum
+      newCell.textContent = "Total: " + total;
+    });
   
 };
 
